c71a503aee14c585051d72e9796b91773f66bbde
Kieue Caprie <103897346+KieueCaprie@users.noreply.github.com>
2026-09-04 23:13:22 +0100

    Update satellaviewplus.sh
    Adds a check for config.json, adds a recurring check for bs-x.sfc for initial setup.

--- satellaviewplus.sh
+++ satellaviewplus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Where are we? Find out.
+# Get script directory.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Check for Satellaview+
 if [ -e "$SCRIPT_DIR"/Satellaview+*.appimage ]
@@ -12,6 +12,7 @@ else
     echo "Marking appimage as executable (chmod +x Satellaview+.appimage)"
     chmod +x Satellaview+.appimage
 fi
+
 # Check for SuperSNES9x
 if [ -e "$SCRIPT_DIR"/SuperSNES9x.appimage ]
 then
@@ -22,21 +23,29 @@ else
     echo "Marking appimage as executable (chmod +x SuperSNES9x.appimage)"
     chmod +x SuperSNES9x.appimage
 fi
+
+# Check for config for Satellaview+, automate building one if there is none to be found.
+# This is to ensure the least amount of pain from setup as humanly possible.
+if [ -e "$SCRIPT_DIR"/config.json ]
+then
+    echo "Satellaview+ Config exists. Continuing."
+else
+    echo "Satellaview+ Config does not exist. Creating."
+    jq -n --arg dl ""$SCRIPT_DIR"" --arg emu ""RetroArch"" --argjson strtmin false --argjson quitonclose true '{DownloadLocation: $dl, Emulator: $emu, StartMinimized: $strtmin, ExitOnClose: $quitonclose}' > config.json
+fi
+
 # Satellaview+ - Needs a few moments to download satdata
 echo "Booting up Satellaview+..."
 # Original boot, use if launching directly as opposed to a terminal or 3rd party launcher
 # ./Satellaview+_V5.appimage
 "$SCRIPT_DIR"/Satellaview+_V5.appimage &
-sleep 10
+
+# Wait until bs-x.sfc is available.
+# If setup went correctly, Satellaview+ will download the RetroArch version, which includes the vaunted bs-x.sfc that's included into the satdata.
+# Using any other method will cause the program to delete BS-X.sfc everytime and allows us to not use RetroArch.
+until [ -e "$SCRIPT_DIR"/roms/bs-x/bs-x.sfc ]; do
+    sleep 1
+done
 # Boot BS-X - Much more simpler.
 echo "Beginning boot up for BS-X..."
-# Check to see if BS-X is in satdata, this is because Satellaview+ tends to delete bs-x.sfc for some unknown reason...
-if [ -e "$SCRIPT_DIR"/satdata/BS-X.sfc ]
-then
-    echo "BS-X.sfc exists. Launching SuperSNES9x"
-else
-    echo "BS-X.sfc does not exist. Copying..."
-    cp "$SCRIPT_DIR"/BS-X.sfc "$SCRIPT_DIR"/satdata/BS-X.sfc
-fi
-# ./supersnes9x.appimage "./satdata/BS-X.sfc"
-"$SCRIPT_DIR"/SuperSNES9x.appimage ./satdata/BS-X.sfc
+"$SCRIPT_DIR"/SuperSNES9x.appimage ./roms/bs-x/bs-x.sfc
